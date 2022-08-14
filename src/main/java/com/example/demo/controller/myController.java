013ac8fe19e0cffad06cbe2d71b6ca439f942433
yuba <571239193@qq.com>
2022-08-14 14:52:38 +0800

    commit

--- src/main/java/com/example/demo/controller/myController.java
+++ src/main/java/com/example/demo/controller/myController.java
@@ -1,26 +1,32 @@
 package com.example.demo.controller;
 
+import lombok.Data;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
+import javax.annotation.Resource;
 
+@Data
 @Controller
 public class myController {
-
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
     @RequestMapping("/index")
     public String index() {
         return "index";
     }
-
     @RequestMapping("/login")
     public String login() {
         Logger logger =  LoggerFactory.getLogger(myController.class);
         logger.info("用户访问了一次登陆界面");
+
         System.out.println("hell word!");
+
         return "login";
     }
 
