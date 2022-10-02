ae0c618e6d04d89614c7f056027c22d66ed37761
yuba <571239193@qq.com>
2022-10-02 18:56:57 +0800

    修正5

---
+++ src/main/java/com/example/demo/service/UserAuthService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.UserDetil;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+
+public class UserAuthService implements UserDetailsService {
+
+    @Resource
+    UserMapper mapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetil data = mapper.selectByName(username);
+        if(data == null) throw new UsernameNotFoundException("用户 "+username+" 登录失败，用户名不存在！");
+        return User
+                .withUsername(data.getName())
+                .password(data.getPassword())
+                .roles("user")
+                .build();
+    }
+}
+
