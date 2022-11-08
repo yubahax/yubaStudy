47459276524eb455b27317ac445d40ccc4e8ff98
yuba <571239193@qq.com>
2022-11-08 16:11:18 +0800

    部分修改

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
 
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +11,24 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("book")
 public class Book {
+
+    @TableId(type = IdType.AUTO)
     private int bid;
+
+    @TableField("title")
     private String title;
+
+    @TableField("author")
     private String author;
+
+    @TableField("address")
     private String address;
+
+    @TableField("type")
     private String type;
+
+    @TableField("isalive")
     private int isalive;
 }
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -9,10 +9,8 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-
     @Select("select * from user where name = #{name}")
     User selectByName(@Param("name") String name);
-
     @Insert("insert into user(name,password,role,email) values(#{name},#{password},#{role},#{email})")
     void saveUser(User user);
     @Update("UPDATE user SET name = #{name}, password=#{password}, email= #{email} where id = #{id}")
